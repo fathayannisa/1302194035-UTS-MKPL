4864f9b04d98d2bf0b0ef68d1a7b06f2a4d27359
fathayannisa <fathayannisa@student.telkomuniversity.ac.id>
2022-04-20 21:48:59 +0700

    Menghapus dan melakukan refactoring Large class
    Menghapus bad small dan melakukan refactoring pada class Employee menjadi  4 class yaitu class GajiPegawai, DataPribadi, KeluargaPegawai, TahunPegawai

---
+++ TaxCalculator/src/lib/DataPribadi.java
@@ -0,0 +1,10 @@
+package lib;
+
+public class DataPribadi extends Employee{
+    private String employeeId;
+	private String firstName;
+	private String lastName;
+	private String idNumber;
+	private String address; 
+
+}
---
+++ TaxCalculator/src/lib/TahunPegawai.java
@@ -0,0 +1,11 @@
+package lib;
+
+public class TahunPegawai extends Employee{
+
+    private int yearJoined;
+	private int monthJoined;
+	private int dayJoined;
+	private int monthWorkingInYear;
+
+    
+}
