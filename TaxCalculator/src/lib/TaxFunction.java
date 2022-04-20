6793e5d0caca3a80e49a4e18017d9c16a8f50c59
fathayannisa <fathayannisa@student.telkomuniversity.ac.id>
2022-04-20 23:23:28 +0700

    Refactoring Long Method
    Membuat beberapa fungsi dalam class taxFunction

--- TaxCalculator/src/lib/TaxFunction.java
+++ TaxCalculator/src/lib/TaxFunction.java
@@ -19,24 +19,31 @@ public class TaxFunction {
 		
 		int tax = 0;
 		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
 		if (tax < 0) {
 			return 0;
 		}else {
 			return tax;
+		}
+	}
+
+	public static void bulan(int numberOfMonthWorking){
+		if (numberOfMonthWorking > 12) {
+			System.err.println("More than 12 month working per year");
+		}
+		
+	}
+
+	public static void anak(int numberOfChildren){
+		if (numberOfChildren > 3) {
+			numberOfChildren = 3;
+		}
+	}
+		
+	public static void nikah(boolean isMarried, int tax, int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, int numberOfChildren) {
+		if (isMarried) {
+			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
+		}else {
+			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
 		}
 			 
 	}
